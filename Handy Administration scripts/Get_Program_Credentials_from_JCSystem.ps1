bebe651cc8fca29a5e3ccd84c7744f3d10a944a4
Gal Rozman <112376660+ThePinkPanther96@users.noreply.github.com>
2022-10-18 18:42:21 +0300

    Update Get_Program_Credentials_from_JCSystem.ps1

--- Handy Administration scripts/Get_Program_Credentials_from_JCSystem.ps1
+++ Handy Administration scripts/Get_Program_Credentials_from_JCSystem.ps1
@@ -1,3 +1,5 @@
+## This simple script allows you to easily search for a specific program or software that is installed on multiple systems, 
+## for all kinds of administration purposes. ##
 
 #Install-Module JumpCloud
 Import-Module JumpCloud
@@ -10,12 +12,8 @@ $systemsIDs = Get-JCSystem
 foreach ($i in  $systemsIDs ) 
 {
     #write-output $i._id
-    $b =  Get-JCSystemInsights -Table Program -id $i._id|Where-Object {$_.Name -like  "**" } # Enter the name or a part of the name of the program that you want to search
+    $b =  Get-JCSystemInsights -Table Program -id $i._id | Where-Object {$_.Name -like  "**" } # Enter the name or a part of the name of the program that you want to search
     
     write-output  $b.hostname "" $b.version $b.UninstallString 
 
 }
-
-
-
-#Get-JCSystemInsights -Table Program -id  
