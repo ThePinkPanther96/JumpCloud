a6c0c602c4851844e6c69de27c6f7075e8644989
Gal <gal8156@gmail.com>
2023-09-15 15:44:33 +0300

    added JumpCloudBackupScript and Local_Admin_Report

---
+++ Handy Administration scripts/JumpCloudBackupScript.ps1
@@ -0,0 +1,17 @@
+﻿## Note version 1.5.0 or later of the JumpCloud Pwsh module must be installed to leverage this script.
+
+## After running the script five CSV files will be created within the backup location directory specified.
+
+## Populate the below variables before running the script
+
+$BackupLocation = "D:\Backup\JumpCloud Backup" # Enter full path to desired backup folder for backup CSV files. Example path on Mac "/Users/Buster/Backup/JCBackup"
+
+$JumpCloudAPIKey = "ca8a6ab2bfc8ab0d7c56af7cd69443bb536b6bb1" # The JumpCloud API key can be found in the admin console by selecting the drop down icon next to admin email in top right and selecting "API Settings"
+
+## --------------- Do not modify below this line ----------
+
+Set-Location $BackupLocation
+
+Connect-JCOnline -JumpCloudAPIKey $JumpCloudAPIKey -force #Force parameter used to auth to JumpCloud API without update check
+
+Get-JCBackup -All
---
+++ Handy Administration scripts/Local_Admin_Report.ps1
@@ -0,0 +1,59 @@
+﻿#Connectong to the JumpCloud Admin console with API key
+$JumpCloudAPIKey = "6c7e0e03c6715107a01c7083064c7cc8ebc13cac"
+
+Import-Module JumpCloud
+
+#Force parameter used to auth to JumpCloud API without update check 
+Connect-JCOnline -JumpCloudAPIKey $JumpCloudAPIKey -force
+
+#csv credentials 
+$CurrentDate = Get-Date
+$CurrentDate = $CurrentDate.ToString('dd-MM-yyyy')
+$csvpath = "C:\Admins - $CurrentDate.csv"
+
+#inported list settings
+$IT_List = @()
+$sysid = Get-JcSystem
+
+#excluded systems and users
+$Exclude_Users = @("chnadm", "centos", "designadm", "danielt", "galr", "romang", "bobk", "Chain-Reaction571")
+$Exclude_Devices = @("ILTLV-SWRSHAY-LP", "ILTLV-SWKOR-LP", "ILTLV-SWSMICHAL-LP", 
+"ILTLV-SWMUDI-LP", "ILYK-SWMYAKIR-LP", "SFTP", "ILTLV-SWKOGTAL-LP", "ILTLV-SWEELINOY-LP",
+"ILTLV-LAB-DESK", "ILTLV-SWKNOAM-LP")
+
+foreach ($sid in $sysid) 
+{      
+    
+    $IT_List +=  Get-JCSystemUser -SystemID $sid._id | Where-Object {
+
+        $_.Administrator -eq "True" `
+        -and $_.DisplayName -like "*" `
+        -and $_.Username -notin $Exclude_Users `
+        -and $_.DisplayName -notin $Exclude_Devices `
+    }   
+
+}
+
+$IT_List | Export-Csv -Path $csvpath 
+
+#chamail credentials
+$password = ConvertTo-SecureString 'Ch756125!' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('chnmail@chainreaction11.onmicrosoft.com', $password)
+
+#message settings 
+$MailBody = "Please find the attached file to see a list of systems with local admins enabled.
+
+Cahin-Reaction IT Depaerment."
+
+Send-MailMessage `
+    -SmtpServer smtp.office365.com `
+    -UseSsl `
+    -Port 587 `
+    -From "chnmail@chainreaction11.onmicrosoft.com" `
+    -to "ITTeam@chain-reaction.io" `
+    -Subject "ITAutoMessage - Daily Local Admins Report" `
+    -Credential $credential `
+    -Attachments $csvpath `
+    -Body $MailBody
+
+break
